ea363c29deb6abc3718c15ecbf4d0047a3c1c1b7
VITOR EMANUEL RIBEIRO DE ARAÚJO <vitor_e_araujo@estudante.sesisenai.org.br>
2023-06-21 16:57:15 -0300

    incial commit

---
+++ logica/exercicio7/script.js
@@ -0,0 +1,62 @@
+
+// let senha
+
+// senha = Number(prompt("Digite sua senha: "))
+
+// if(senha == 1234){
+//     alert("acesso permitido")
+// }else{
+//     alert("acesso negado")
+// }
+
+
+// let n1, n2
+
+// n1 = Number(prompt("Digite o primeiro numero: "))
+// n2 = Number(prompt("Digite o segundo numero: "))
+
+// if(n1 > n2){
+//     alert(n1)
+// }else{
+//     alert(n2)
+// }
+
+
+// let placa 
+// placa = Number(prompt("Digite o último número da placa: "))
+// if(placa == 0 || placa == 1){
+//     alert("segunda")
+// }
+// if(placa == 2 || placa == 3){
+//     alert("Terça")
+// }
+// if(placa == 4 || placa == 5){
+//     alert("Quarta")
+// }
+// if(placa == 6 || placa == 7){
+//     alert("Quinta")
+// }
+// if(placa == 8 || placa ==9){
+//     alert("Sexta")
+// }
+
+
+// let palestra 
+// palestra = Number(prompt("Digite o numero da palestra: "))
+// if(palestra == 1){
+//     alert("Animação com Scratch, laboratório 305, 19h")
+// }
+// if(palestra == 2){
+//     alert("Scratch para gamers, laboratório 512, 20h")
+// }
+// if(palestra == 3){
+//     alert("JavaScript para leigos, laboratório 101, 19h")
+// }
+// if(palestra == 4){
+//     alert("Tópicos avançados do JavaScript, laboratório 305, 20h")
+// }
+// if(palestra == 5){
+//     alert("Vida e carreira, auditorio")
+// }
+
+
---
+++ logica/saideira/script.js
@@ -0,0 +1,49 @@
+// let media, n1, n2
+
+// n1 = Number(prompt("Digite a sua primeria nota: "))
+// n2 = Number(prompt("Digite a segundo nota: "))
+
+// media = (n1 + n2) / 2
+
+// if(media > 7){
+//     alert("Aprovado")
+// }else{
+//     alert("Reprovado")
+// }
+
+
+// let pontuação
+
+// pontuação = Number(prompt("Digite sua pontuação: "))
+
+// if(pontuação <= 10){
+//      alert("Deu ruim!")
+
+// }
+// if(pontuação > 10 && pontuação <= 100){
+//     alert("mandou bem")
+// }
+// if(pontuação >100 && pontuação <= 200){
+//     alert("Supimpa!")
+// }
+// if(pontuação > 200)
+//     alert("MITOU!")
+
+
+// let dias,diarias, multa, conta
+
+// dias = Number(prompt("Quantos dias vai ficar? "))
+// desconto = Number(0.25)
+// multa = Number(150)
+
+// if(dias <= 5){
+//     diarias = (100*dias)
+// }
+// if(dias >5 && dias <= 10){
+//     diarias = (90*dias) 
+// }
+// if(dias >10){
+//     diarias = (80*dias) 
+// } 
+// conta = diarias*0.75 + multa
+// alert("Conta: " + conta)
